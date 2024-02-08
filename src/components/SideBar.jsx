c8b956e5fa157740411a6709344508493d594c75
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-02-08 15:22:05 +0000

    SideBar の整理

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,12 @@
 export default function Sidebar({children, onClick, projects}){
   return (
-    <div className="left-0 top-0 bottom-0 bg-black mt-16 rounded-tr-lg w-48 flex flex-col items-start">
-      <div className="mt-16 ml-4">{children}</div>
-      <button className="text-stone-400 bg-stone-700 px-4 py-2 rounded mt-10 text-xs ml-4" onClick={onClick}>
+    <div className="bg-black mt-16 rounded-tr-lg w-48 flex flex-col items-start">
+      <h1 className="mt-16 ml-4">{children}</h1>
+      <button className="text-stone-400 bg-stone-700 px-4 py-2 rounded mt-10 text-xs ml-4 mb-6" onClick={onClick}>
         + Add Project
       </button>
       {projects.map(project => (
-      <button key={project.title} className="text-stone-400 bg-stone-700 px-4 py-2 rounded mt-10 text-xs ml-4">
+      <button key={project.title} className="text-stone-300 text-left px-4 w-10/12 mt-2 ml-4 hover:bg-stone-900">
         {project.title}
       </button>))}
     </div>
