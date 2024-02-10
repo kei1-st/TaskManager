a7c156669e837894828de46c7bfc45b884a07785
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-02-10 13:29:23 +0000

    add css to Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,13 @@
 export default function Project({project}){
   return (
-    <div>
-      <h1>{project.title}</h1>
-      <p>{project.due}</p>
-      <p>{project.description}</p>
+    <div className='flex flex-col h-screen mt-20 mx-5'>
+      <div className='flex flex-row justify-between items-center'>
+        <h1 className='text-3xl font-extrabold text-stone-600'>{project.title}</h1>
+        <button className='items-end text-stone-600'> Delete </button>
+      </div>
+      <p className='mt-3 text-stone-400'>{project.due}</p>
+      <p className='mt-3 text-stone-700'>{project.description}</p>
+      <hr className='h-1 my-6 rounded bg-stone-300'/>
     </div>
   )
 }
