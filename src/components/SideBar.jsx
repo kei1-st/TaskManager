a5660fa0cfb2d71a4f1318ad751ad4395b211887
Keiichi Sato <51529803+kei1-st@users.noreply.github.com>
2024-02-11 09:42:45 +0000

    minor fix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar({children, onClickCreate, onClickProject, projects}){
   return (
-    <div className="bg-black mt-16 rounded-tr-lg w-48 flex flex-col items-start">
+    <div className="bg-black mt-16 rounded-tr-lg w-1/4 flex flex-col items-start">
       <h1 className="mt-16 ml-4">{children}</h1>
       <button className="text-stone-400 bg-stone-700 px-4 py-2 rounded mt-10 text-xs ml-4 mb-6" onClick={onClickCreate}>
         + Add Project
